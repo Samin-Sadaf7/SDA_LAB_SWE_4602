95c1244aaad08f52d9630a25c262c0abd9c42ee9
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-03-27 21:36:08 +0600

    Micoroservice Updated

--- API-GATEWAY/API-GATEWAY/src/main/java/com/example/APIGATEWAY/ApiGatewayApplication.java
+++ API-GATEWAY/API-GATEWAY/src/main/java/com/example/APIGATEWAY/ApiGatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -14,4 +14,4 @@ public class ApiGatewayApplication {
 
 }
 
-//            uri: http://localhost:9002/employees/ (in application.yml)
+
