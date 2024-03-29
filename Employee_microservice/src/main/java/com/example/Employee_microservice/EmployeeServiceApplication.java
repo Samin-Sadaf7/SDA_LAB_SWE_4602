10de8335585645f695966941cc38c084c5d095be
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-03-29 11:21:38 +0600

    added new code

--- Employee_microservice/src/main/java/com/example/Employee_microservice/EmployeeServiceApplication.java
+++ Employee_microservice/src/main/java/com/example/Employee_microservice/EmployeeServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
